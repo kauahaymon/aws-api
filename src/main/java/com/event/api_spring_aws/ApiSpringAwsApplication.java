da011c9cda2b978e311bcad53831ec1886227ad1
Kauã Haymon <kauahaymon@hotmail.com>
2024-09-16 13:12:43 -0400

    first commit

---
+++ src/main/java/com/event/api_spring_aws/ApiSpringAwsApplication.java
@@ -0,0 +1,14 @@
+package com.event.api_spring_aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringAwsApplication.class, args);
+		System.out.println("hi world");
+	}
+
+}
---
+++ src/test/java/com/event/api_spring_aws/ApiSpringAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event.api_spring_aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
